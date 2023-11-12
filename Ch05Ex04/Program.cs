ed7def62628d84091aa9452e91da9a761df16a06
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-12 15:20:04 +0100

    Add project files.

---
+++ Ch05Ex04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ch05Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Anthony", "Mary Chris", "Autry Rual" };
+
+            Console.WriteLine($"Here are {friendNames.Length} of my friends:");
+            foreach (string friendName in friendNames)
+            {
+                Console.WriteLine(friendName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
